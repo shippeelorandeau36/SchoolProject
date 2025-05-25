90f54462870795347f7e6828691dd939bc9ba162
shippeelorandeau36 <shippeelorandeau36@gmail.com>
2025-05-25 10:04:02 +0000

    Add 71.ts

---
+++ src/71.ts
@@ -0,0 +1,9 @@
+let x = 5;
+if (x > 10) {
+    console.log("x is greater than 10");
+}
+else if (x < 5) {
+    console.log("x is less than 5");
+} else {
+    console.log("x is not greater nor less than 10");
+}
